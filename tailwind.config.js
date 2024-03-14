7225416e9c42844e407be88a8634296a0a095991
laxmantako12 <laxmantako12@gmail.com>
2024-03-14 23:38:38 +0545

    About page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html","./dist/contact.html"],
+  // content: ["./dist/index.html","./dist/contact.html"],
+  content: ['./dist/**/*.{html,js}'],
   
   theme: {
     extend: {},
